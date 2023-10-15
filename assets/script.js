3ea9e19e6b5a7cc1804799c6e5cb572ea5b5b398
Roro21007 <rharris21007@gmail.com>
2023-10-14 19:41:59 -0500

    add start and next functions in JS

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,49 @@
 /*multiple choice
 /*score
 /*scoreboard/initials
-/*timer
+timer
 /*start button
 /*new question
-/*game over
+/*game over*/
+
+var timerEl = document.querySelector("#timer");
+
+var score = 0;
+
+var secondsLeft = 300;
+
+var startEl = document.querySelector("#start");
+var nextEl = document.querySelector("#next")
+
+function setTime() {
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timerEl.textContent = secondsLeft;
+
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+            sendMessage();
+        }
+    }, 1000);
+}
+
+function nextQuestion() {
+    /*question*/
+}
+
+function sendMessage() {
+    timerEl.textContext = "Game Over";
+}
+
+startEl.addEventListener("click", function() {
+    setTime();
+});
+
+nextEl.addEventListener("click", function() {
+    if (secondsLeft > 0) {
+        nextQuestion()
+    } else {
+        sendMessage();
+    }
+})
+
